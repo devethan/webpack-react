bf050f7fbd4837d99110b218539804704f7fa937
devohno <devohno.ko@gmail.com>
2019-12-12 15:14:55 +0900

    3. Building ReactJS using webpack
    1) Create React file in 'src/'
    
    2) Set 'webpack.config.js' with 'babel-loader'
    
    3) Set '.babelrc' in Root directory

---
+++ src/Root.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Root = () => {
+    return (
+        <h3>Hello, React</h3>
+    );
+};
+
+export default Root;
--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,37 @@ const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/test.js",
+  entry: "./src/index.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname + "/build")
   },
   mode: "none", // production, development, none
   module: {
-    rules: [{
-      test: /\.html$/, // regular expression which to test
-      use: [
-        {
-            // Loader can help Webpack to understand by excuting 'html-loader' when read HTML file
-          loader: "html-loader", // name of loader
-          options: {
-            // options of loader
-            // minimize: true
+    rules: [
+        // 'babel-loader' can change javascript files written on ES6 to on ES5
+        // It can be read in browsers which not supported ES6
+      {
+        test: /\.(js|jsx)$/,
+        exclude: "/node_modules",
+        use: ["babel-loader"]
+      },
+      // Loader can help Webpack to understand by excuting 'html-loader' when read HTML file
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: {
+              minimize: true
+            }
           }
-        }
-      ]
-    }]
+        ]
+      }
+    ]
   },
   plugins: [
-      // HtmlWebPackPlugin imports the 'bundle.js' to 'index.html' automatically when building webpack. 
+    // HtmlWebPackPlugin imports the 'bundle.js' to 'index.html' automatically when building webpack.
     new HtmlWebPackPlugin({
       template: "./public/index.html", // read
       filename: "index.html" // write
