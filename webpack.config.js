7524fe4865b3052b781c601de86d88c49d5b7d76
devohno <devohno.ko@gmail.com>
2019-12-12 14:21:56 +0900

    2. Building HTML file using webpack
    1) Create HTML file in 'public/index.html'
    
    2) Set 'webpack.config.js' with 'html-webpack-plugin' and 'html-loader'

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/test.js",
@@ -6,5 +7,27 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname + "/build")
   },
-  mode: "none"  // production, development, none
+  mode: "none", // production, development, none
+  module: {
+    rules: [{
+      test: /\.html$/, // regular expression which to test
+      use: [
+        {
+            // Loader can help Webpack to understand by excuting 'html-loader' when read HTML file
+          loader: "html-loader", // name of loader
+          options: {
+            // options of loader
+            // minimize: true
+          }
+        }
+      ]
+    }]
+  },
+  plugins: [
+      // HtmlWebPackPlugin imports the 'bundle.js' to 'index.html' automatically when building webpack. 
+    new HtmlWebPackPlugin({
+      template: "./public/index.html", // read
+      filename: "index.html" // write
+    })
+  ]
 };
